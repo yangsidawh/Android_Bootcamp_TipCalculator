9a81913523749d92f820c2f11e453683e74cd1f2
Sida Yang <yangsidawh@gmail.com>
2014-09-06 19:28:30 -0700

    project initial

---
+++ TipCalculator/gen/com/yahoo/sida/tipcalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.yahoo.sida.tipcalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TipCalculator/src/com/yahoo/sida/tipcalculator/TipCalcActivity.java
@@ -0,0 +1,15 @@
+package com.yahoo.sida.tipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TipCalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calc);
+    }
+}
